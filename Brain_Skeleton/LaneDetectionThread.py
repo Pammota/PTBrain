83eae06891cd7cf1e770f8c532c385a9d434b45a
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-01-09 21:11:45 +0200

    Update LaneDetectionThread.py

--- Brain_Skeleton/LaneDetectionThread.py
+++ Brain_Skeleton/LaneDetectionThread.py
@@ -30,7 +30,7 @@ class LaneDetectionThread(Thread):
 
 
     def preprocessing(self, frame):
-        frame_copy = frame[int(int(frame.shape[0] * 0.7)):, :]
+        frame_copy = frame[int(int(frame.shape[0] * 0.65)):, :]
 
         frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
 
@@ -126,7 +126,7 @@ class LaneDetectionThread(Thread):
                                     maxLineGap=100)
             left_lanes = []
             right_lanes = []
-            frame_copy = frame[int(frame.shape[0] * 0.7):, :]  # used for displaying
+            frame_copy = frame[int(frame.shape[0] * 0.65):, :]  # used for displaying
 
             if lines is not None:
                 # classify lanes based on their slope
