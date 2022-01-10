db740cc4cf9279de1ce3a6a1dea61f2db022b3d2
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-01-10 02:12:38 +0200

    Update LaneDetectionThread.py

--- Brain_Skeleton/LaneDetectionThread.py
+++ Brain_Skeleton/LaneDetectionThread.py
@@ -198,6 +198,7 @@ class LaneDetectionThread(Thread):
             # self.writer.write(frame)
             addition = np.zeros((640, 60, 3), dtype=np.uint8)
             cv2.putText(addition, str(theta_average), (0, 0), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2)
+            frame = cv2.resize(frame, (640, 480))
             frame = cv2.hconcat(addition, frame)
             self.list_of_frames.append(frame)
             print("theta_average = " + str(theta_average))
