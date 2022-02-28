118665b1858b6a2784378c0aa040d2d59d1b7a12
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-28 22:12:23 +0200

    Update Controller.py

--- Brain_Skeleton/Controller.py
+++ Brain_Skeleton/Controller.py
@@ -3,7 +3,7 @@ class Controller:
     def __init__(self, time_threshold=0.15):
         self.speed = 0
         self.angle = 0
-        self.p = 0.5
+        self.p = 0.1
         self.d = 0.0001
         self.last_error = 0
         self.time_threshold = time_threshold
