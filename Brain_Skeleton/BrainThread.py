89b21ed51fd6d3ed4c90e4dac382107cc3f675ca
TudorTrasculescu123 <tudortrasculescu000@gmail.com>
2022-03-11 21:07:29 +0200

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -178,7 +178,7 @@ class BrainThread(Thread):
         speed = 20
         theta_command = self.controller.update_angle(theta)
         speed_command, startup = self.controller.update_speed(speed)
-        for r in range(210):
+        for r in range(280):
             self.outP_com.send((theta_command, speed_command))
             time.sleep(0.025)
         time.sleep(0.05)
