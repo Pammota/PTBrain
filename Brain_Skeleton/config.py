eac2de6e844114f346f276f20d1ebf7cacb31452
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-13 19:16:44 +0200

    added the augmented model

--- Brain_Skeleton/config.py
+++ Brain_Skeleton/config.py
@@ -7,15 +7,14 @@ CLASSES = {
     3: {"LABEL": "Parking", "COLOR": (0, 0, 255)},
     4: {"LABEL": "Sem_Yellow", "COLOR": (255, 0, 0)},
     5: {"LABEL": "Sem_Red", "COLOR": (255, 0, 0)},
-    6: {"LABEL": "Sem_No_Light", "COLOR": (255, 255, 255)},
-    7: {"LABEL": "Sem_Green", "COLOR": (0, 255, 0)},
-    8: {"LABEL": "Priority", "COLOR": (0, 255, 0)},
-    9: {"LABEL": "Crosswalk", "COLOR": (0, 0, 255)},
-    10: {"LABEL": "Stop", "COLOR": (255, 0, 0)},
+    6: {"LABEL": "Sem_Green", "COLOR": (0, 255, 0)},
+    7: {"LABEL": "Priority", "COLOR": (0, 255, 0)},
+    8: {"LABEL": "Crosswalk", "COLOR": (0, 0, 255)},
+    9: {"LABEL": "Stop", "COLOR": (255, 0, 0)},
 
 }
 
-DETECTION_SCORE_THRESHOLD = 0
+DETECTION_SCORE_THRESHOLD = 0.1
 IOU_THRESHOLD = 0.1
 
 CLASSIFIER_INPUT_SHAPE = (224, 224)
