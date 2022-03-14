b025dd1dcc40b4935cb2e2c85cd532766819fb47
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-14 22:26:07 +0200

    added a better control class

--- Brain_Skeleton/Control.py
+++ Brain_Skeleton/Control.py
@@ -1,36 +1,44 @@
-
+import numpy as np
 
 class Control():
-    def __init__(self, OD_info, LD_info):
+    def __init__(self):
         #call the GraphPars for path planning based on the maneuvers we want
-        #AiPrioritate, Stop, Crosswalk, Priority(cedeaza), Park, RED, YELLOW, Green, Intersection1, Intersection0
-        flags = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        states = ["IDLE", "Decision center", "Crossroads", "Stop", "Left_maneuver", "Right_maneuver", "Lane Follow", "Parking"]
-        curr_state = "IDLE"
-        self.Check_State(OD_info, LD_info, flags, states, curr_state)
-
-    def Check_State(self, OD_info, LD_info, flags, states, curr_state):
-       self.Set_Flags(OD_info, LD_info, flags)
-       if curr_state == "IDLE":
-           curr_state = "Decision center"
-       elif curr_state == "Decision center":
-           if flags[8] == 1 and flags[9] == 1:  ## we are at the begining of the intersection
-               curr_state = "Crossroads"
-           else:
-               curr_state = "Lane Follow"
-       elif  curr_state == "Crossroads":
-           if flags[1] == 1 or flags[3] == 1 or flags[5] == 1: ## obj detection gave some stops
-               curr_state = "Stop"
-           elif flags[8] == flags[9] == 0:
-               curr_state = ""
-       elif curr_state == "Stop":
-           ##logic for making the car stop for 2 or 3 seconds
-           flags[1] = flags[3] = flags[5] = 0 #make sure the car won't stop again on the next step
-           curr_state = "Crossroads"
-       #elif curr_state == ""
+        # flags given by oobject detection -
+        self.flags = {}
+        self.state = "Lane Follow"
+        self.action = {"speed": 0, "angle": 0, "stop": False, "red": False, "direction": "forward",
+                       "crosswalk": False, "parking": False}
+        self.directions = ["forward"]
+        self.dir_idx = 0
+        self.had_parking = False
+        self.base_speed = 20
+        self.theta = 0
+        self.thetas = []
+        self.passed_horiz_line = False
+        self.ongoing_intersection = False
 
 
+    def CheckState(self, OD_info, LD_info):
+        self.SetFlags(OD_info)
+        self.SetTheta(LD_info)
+        self.passed_horiz_line = False
 
-    def Set_Flags(self, OD_info, LD_info, flags):
-        #flags.... -> set here
-        a=0
+        if self.state == "Lane Follow":
+            if self.passed_horiz_line == True:
+                self.state = "Intersection"
+                self.ongoing_intersection = True
+
+        if self.state == "Intersection":
+            if self.ongoing_intersection == False:
+                self.state = "Lane Follow"
+                self.dir_idx += 1
+
+
+    def SetFlags(self, OD_info):
+        self.flags = OD_info
+
+    def SetTheta(self, LD_info):
+        self.thetas.append(LD_info["theta"])
+        if len(self.thetas) == 3:
+            self.theta = np.average(self.thetas)
+            self.thetas = []
