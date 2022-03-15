3fc450f961bbbe9a786addbd607b8d5861974b49
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-15 22:05:37 +0200

    angle / 2

--- Brain_Skeleton/LaneDetectionThread.py
+++ Brain_Skeleton/LaneDetectionThread.py
@@ -311,7 +311,7 @@ class LaneDetectionThread(Thread):
         if vp_exists:
             line_vp = self.get_inverse_line_IPM([y_cv_IPM_vp, x_cv_IPM_vp, int(self.width_ROI_IPM / 2 + self.offset_origin), self.height_ROI_IPM], frame_ROI)
             self.draw_line(line_vp, (255, 255, 255), frame_ROI)
-            theta = round(math.degrees(math.atan((self.y_cv_IPM_center - y_cv_IPM_vp) / (self.height_ROI_IPM - x_cv_IPM_vp))))
+            theta = int(round(math.degrees(math.atan((self.y_cv_IPM_center - y_cv_IPM_vp) / (self.height_ROI_IPM - x_cv_IPM_vp)))) / 2)
             if theta > 23:
                 theta = 23
             if theta < -23:
