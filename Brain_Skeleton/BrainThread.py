72bb12a324dfc7af9d7d6641f157d79dd7b6582b
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-15 19:34:33 +0200

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -29,7 +29,7 @@ class BrainThread(Thread):
         self.cameraSpoof = cameraSpoof
         self.camera = cv2.VideoCapture(0 if cameraSpoof is None else cameraSpoof)
 
-        self.baseSpeed = 14
+        self.baseSpeed = 17
 
         self.show_vid = show_vid
         self.show_lane = show_lane
