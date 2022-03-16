ba1a52096ac071bb1c72e5210f94e98d0c6129a8
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-16 19:53:42 +0200

    updated logic

--- Brain_Skeleton/Controller.py
+++ Brain_Skeleton/Controller.py
@@ -34,8 +34,7 @@ class Controller():
                     self.setExecuted(parking=False, crosswalk=False)
                     self.state = "Intersection"
                     self.ongoing_intersection = True
-
-        if self.state == "Intersection":
+        elif self.state == "Intersection":
             if not self.ongoing_intersection:
                 self.state = "Lane Follow"
                 self.dir_idx += 1
@@ -46,12 +45,11 @@ class Controller():
         if self.state == "Lane Follow":
             if self.flags["parking"] is True:
                 self.had_parking = True
-            else:
-                if self.flags["crosswalk"] and self.passed_horiz_line and not self.executed["crosswalk"]:
-                    self.passed_horiz_line = False
-                    self.setExecuted(crosswalk=True)
-                    return [0, 0, 0, 0, 0, 1, 0]  # activate crosswalk flag
-            if self.had_parking is True and self.flags["parking"] is False and not self.executed["parking"]:
+            elif self.flags["crosswalk"] and self.passed_horiz_line and not self.executed["crosswalk"]:
+                self.passed_horiz_line = False
+                self.setExecuted(crosswalk=True)
+                return [0, 0, 0, 0, 0, 1, 0]  # activate crosswalk flag
+            elif self.had_parking is True and self.flags["parking"] is False and not self.executed["parking"]:
                 self.setExecuted(parking=True)
                 self.had_parking = False
                 return [0, 0, 0, 0, 0, 0, 1]  #activate parking flag
