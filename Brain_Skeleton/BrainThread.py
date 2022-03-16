23eacc1c2f04e8a66271a5d43e6291ea4464cbb5
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-16 20:57:57 +0200

    a

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -222,6 +222,7 @@ class BrainThread(Thread):
         self.hardcoded_move(22.9, 46, 38, 0.02)
         self.hardcoded_move(0, 46, 25, 0.02)
         time.sleep(0.02)
+        self.hardcoded_move(0, 14, 5, 0.001)
         # self.hardcoded_move(0, 0, 10, 0.001)
 
 
