9046f1495a779543881bd88a7678ccbfce07709f
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-16 17:45:57 +0200

    Update LaneDetectionThread.py

--- Brain_Skeleton/LaneDetectionThread.py
+++ Brain_Skeleton/LaneDetectionThread.py
@@ -392,9 +392,9 @@ class LaneDetectionThread(Thread):
                         color=(0, 255, 0), thickness=3)
 
             # cv2.imshow("Frame", frame)
-            #cv2.imshow("ROI", frame_ROI)
-            cv2.imshow("IPM", frame_ROI_IPM)
-            #cv2.waitKey(1)
+            cv2.imshow("ROI", frame_ROI)
+            # cv2.imshow("IPM", frame_ROI_IPM)
+            cv2.waitKey(1)
 
             end = time.time()
             if config.PRINT_EXEC_TIMES:
