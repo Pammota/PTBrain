dbd2f7688eac2a75653b6f23a0e0124ac34dbdb6
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-16 22:45:04 +0200

    left intersection maneuver

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -198,11 +198,11 @@ class BrainThread(Thread):
 
 
     def left_maneuver_routine(self):
-        self.hardcoded_move(0, 23, 20, 0.05)
+        self.hardcoded_move(0, 23, 5, 0.05)
         time.sleep(0.05)
-        self.hardcoded_move(-12.7, 20, 250, 0.025)
+        self.hardcoded_move(-19, 17, 270, 0.025)
         time.sleep(0.025)
-        self.hardcoded_move(0, 0,20, 0.001)
+        self.hardcoded_move(0, 13, 10, 0.001)
 
 
     def parking_maneuver(self):
