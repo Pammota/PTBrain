dca69232366f0e1dc029a4a007ecffbaeb69fb6a
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-19 01:27:35 +0200

    added distance sensor reader

--- Brain_Skeleton/readthread.py
+++ Brain_Skeleton/readthread.py
@@ -9,7 +9,7 @@ class ReadThread(Thread):
 
         super(ReadThread, self).__init__()
 
-        devFile = '/dev/ttyAMA0'
+        devFile = '/dev/ttyACM0'
 
         # comm init
         self.serialCom = serial.Serial(devFile, 256000, timeout=0.003)
