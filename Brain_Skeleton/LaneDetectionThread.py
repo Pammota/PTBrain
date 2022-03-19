6448bc6f668ee2c52015cff31ac569209439e03a
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-20 01:09:58 +0200

    fuck

--- Brain_Skeleton/LaneDetectionThread.py
+++ Brain_Skeleton/LaneDetectionThread.py
@@ -139,8 +139,21 @@ class LaneDetectionThread(Thread):
         # coordinates used for estimating our line
         x_points = []
         y_points = []
+        min_x_cv = self.height_ROI
+        max_x_cv = 0
 
         for line in lines:
+            y1_cv, x1_cv, y2_cv, x2_cv = line[0]
+            if x1_cv < min_x_cv:
+                min_x_cv = x1_cv
+            if x2_cv < min_x_cv:
+                min_x_cv = x2_cv
+            if x1_cv > max_x_cv:
+                max_x_cv = x1_cv
+            if x2_cv > max_x_cv:
+                max_x_cv = x2_cv
+            if abs(max_x_cv - min_x_cv) > 170:
+                return None
             x1, y1, x2, y2 = self.get_XoY_coordinates(line)
             x_points.append(x1)
             x_points.append(x2)
