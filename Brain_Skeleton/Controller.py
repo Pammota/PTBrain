1b2778cb7926fcd4ba086e3fcf32d7e3aba989ce
flaviusfetean <flavius_fetean@yahoo.com>
2022-03-19 23:15:01 +0200

    disabled distance sensor unless in a crosswalk

--- Brain_Skeleton/Controller.py
+++ Brain_Skeleton/Controller.py
@@ -8,7 +8,7 @@ class Controller():
                  "sem_green": False, "priority": False, "crosswalk": False, "stop": False}
         self.flags_history = []
         self.state = "Lane Follow"
-        self.directions = ["left", "forward"]
+        self.directions = ["forward", "right"]
         self.dir_idx = 0
         self.had_parking = False
         self.base_speed = 13
