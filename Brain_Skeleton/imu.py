fef6a0260946c92d1453412a8a30822c0ad90213
flaviusfetean <flavius_fetean@yahoo.com>
2022-04-17 01:03:24 +0300

    start imu again

--- Brain_Skeleton/imu.py
+++ Brain_Skeleton/imu.py
@@ -79,7 +79,7 @@ class imu(threading.Thread):
 
                 true_accelx = self.accelx + math.sin(math.radians(self.pitch))
                 true_accely = self.accely - math.sin(math.radians(self.roll))
-                true_accelz = self.accelz - math.sin(math.radians(self.pitch)) - math.sin(math.radians(self.roll))
+                true_accelz = self.accelz - math.sin(math.radians(self.pitch)) + math.sin(math.radians(self.roll))
                 print("accelx = %f, accely = %f, accelz = %f"%(true_accelx, true_accely, true_accelz))
                 time.sleep(self.poll_interval*1.0/100.0)
 
