97c6fdeb68fe56cb26bee71503901a9b8cdec717
flaviusfetean <flavius_fetean@yahoo.com>
2022-04-17 00:53:40 +0300

    start imu again

--- Brain_Skeleton/imu.py
+++ Brain_Skeleton/imu.py
@@ -77,7 +77,8 @@ class imu(threading.Thread):
 
                 #print("accelx = %f, accely = %f accelz = %f" %(self.accelx, self.accely, self.accelz))
 
-                print("accelx = %f, pitch = %f"%(self.accelx, self.pitch))
+                true_accelx = self.accelx + math.sin(math.radians(self.pitch))
+                print("accelx = %f"%(true_accelx))
                 time.sleep(self.poll_interval*1.0/100.0)
 
     def stop(self): 
