eef620ac23ab5e57acc08a3b36b2a39dcec64467
flaviusfetean <flavius_fetean@yahoo.com>
2022-04-17 22:13:32 +0300

    enhanced map

--- Brain_Skeleton/imu_tracking.py
+++ Brain_Skeleton/imu_tracking.py
@@ -62,5 +62,5 @@ class IMU_tracking(Thread):
             cv2.imshow("map", image)
             cv2.waitKey(1)
 
-            time.sleep(0.03)
-            dt = 30 # s
+            time.sleep(0.04)
+            dt = 40 # s
