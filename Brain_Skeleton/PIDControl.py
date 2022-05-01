5b80a0f2811dfdac461795891a3b5de1c5a0fd62
flaviusfetean <flavius_fetean@yahoo.com>
2022-05-01 18:42:47 +0300

    car tailing

--- Brain_Skeleton/PIDControl.py
+++ Brain_Skeleton/PIDControl.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class PIDControl():
-    def __init__(self, threshold, kp=0.1, ki=0, kd=0.01):
+    def __init__(self, threshold, kp=0.2, ki=0, kd=0.01):
 
         self.threshold = threshold
         self.kp = kp
