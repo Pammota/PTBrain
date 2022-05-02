2e69a9e8c3fd74740af79061c96b8bca5136f2bd
flaviusfetean <flavius_fetean@yahoo.com>
2022-05-03 00:02:30 +0300

    changed pid values

--- Brain_Skeleton/PIDControl.py
+++ Brain_Skeleton/PIDControl.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 
 class PIDControl():
-    def __init__(self, threshold, kp=0.45, ki=0.5, kd=0.25):
+    def __init__(self, threshold, kp=0.5, ki=0.45, kd=0.25):
 
         self.threshold = threshold
         self.kp = kp
