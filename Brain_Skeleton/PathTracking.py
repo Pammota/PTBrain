0838dadfc91167e0a5348bdaad9ddf7bab7e6e91
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-04 19:06:42 +0300

    Update PathTracking.py

--- Brain_Skeleton/PathTracking.py
+++ Brain_Skeleton/PathTracking.py
@@ -113,7 +113,7 @@ class PathTracking:
             if y_ref - y_int >= 0:
                 d = self.distance((x_ref, y_ref), (self.x_car, self.y_car))
                 print("d = {} cm".format(d))
-                if d >= self.ref_thresh and d >= min_dist:
+                if d >= self.ref_thresh and d <= min_dist:
                     print("FOUND REFERENCE POINT TO FOLLOW")
                     point_ref_min = point_ref
                     min_dist = d
