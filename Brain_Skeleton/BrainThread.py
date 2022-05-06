e0cf1c05d46f572269c987123f1bc8a48d67e2fc
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-06 23:09:44 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -226,7 +226,7 @@ class BrainThread(Thread):
         self.path_tracking(case=direction, x_car=x_offset, y_car=y_offset,
                            theta_yaw_map=theta_yaw_map, yaw=yaw,
                            v=14,
-                           dt=0.05)
+                           dt=0.06)
 
         # if direction == "left":
         #     if self.cameraSpoof is None:
