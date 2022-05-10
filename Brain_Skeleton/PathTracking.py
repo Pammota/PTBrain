f22f3dc76a1b0df4d28bee545ba66a7b92fbfa7c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-11 01:20:54 +0300

    Update PathTracking.py

--- Brain_Skeleton/PathTracking.py
+++ Brain_Skeleton/PathTracking.py
@@ -230,7 +230,10 @@ class PathTracking:
                 steering_angle = -23
             print("steering angle = {}".format(steering_angle))
 
-            angle_command = Controller.getAngleCommand(-int(steering_angle * 1.5))
+            if self.isForward == True:
+                angle_command = Controller.getAngleCommand(-int(steering_angle * 2.))
+            else:
+                angle_command = Controller.getAngleCommand(-int(steering_angle * 1.5))
             self.outP_com.send((angle_command, speed_command))
 
             self.x_car = self.x_car + self.v * math.cos(math.radians(self.theta_car + steering_angle)) * self.dt
