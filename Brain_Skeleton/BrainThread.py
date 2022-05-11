eb7b3b356b0cf89499771b1122d3b2ee625d5ff4
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-12 02:46:55 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -533,22 +533,27 @@ class BrainThread(Thread):
         if case == "roundabout_forward":
             # info for roundabout
             x0, y0 = 117, 10
-            ref_thresh = 30
-            final_thresh = 15
+            ref_thresh = 5
+            final_thresh = 5
             isForward = False
             size_pixel = 500
             size_cm = 234
-            ref_points_aux = pathGenerator.generate_circle_points(r=50, d=15, x_c=99, y_c=117, alpha_min=0,
+            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=5, x_c=99, y_c=117, alpha_min=0,
                                                                   alpha_max=1.04)
             for point in ref_points_aux:
                 ref_points.append(point)
-            ref_points_aux = pathGenerator.generate_circle_points(r=70, d=20, x_c=99, y_c=117, alpha_min=5.23,
+            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=5, x_c=99, y_c=117, alpha_min=5.23,
                                                                   alpha_max=6.28)
             for point in ref_points_aux:
                 ref_points.append(point)
             # ref_points = [(117, 40), (140, 80), (140, 120), (87, 184), (67, 214)]
             ref_points.append((117, 40))
-            ref_points.append((87, 214))
+            ref_points.append((117, 30))
+            ref_points.append((117, 20))
+            ref_points.append((117, 204))
+            ref_points.append((117, 194))
+            ref_points.append((117, 184))
+            # ref_points.append((87, 214))
 
 
             # ref_points_aux = pathGenerator.generate_line_points(x1=135, y1=10, x2=135, y2=40, n=3)
