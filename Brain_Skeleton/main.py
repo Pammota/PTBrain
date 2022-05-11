5f6b05141b55de69a4d8c5f646fa33dadc778061
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-11 21:48:58 +0300

    summary

--- Brain_Skeleton/main.py
+++ Brain_Skeleton/main.py
@@ -15,7 +15,6 @@ ag.add_argument('-l', '--show_lane', required=False)
 ag.add_argument('-s', '--stop_car', required=False)
 #ag.add_argument('-m', '--run_mode', required=False)
 args = vars(ag.parse_args())
-imuthread = imu()
 
 if args['path_to_vid'] is None:
     if args['stop_car'] is None:
@@ -28,7 +27,6 @@ else:
     else:
         brain = BrainThread(cameraSpoof=args['path_to_vid'], stop_car=args['stop_car'])
 brain.start()
-imuthread.start()
 
 blocker = Event()
 
