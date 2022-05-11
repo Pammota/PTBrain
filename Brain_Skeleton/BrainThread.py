07f6225cca7171c18cbf2382042f7cc32a8f4ef8
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-12 01:05:52 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -546,7 +546,7 @@ class BrainThread(Thread):
                                                                   alpha_max=6.28)
             for point in ref_points_aux:
                 ref_points.append(point)
-            ref_points = [(117, 40), (140, 80), (140, 120), (77, 184), (107, 214)]
+            ref_points = [(117, 40), (140, 80), (140, 120), (77, 184), (77, 214)]
             ref_points.append((117, 40))
             ref_points.append((117, 214))
 
