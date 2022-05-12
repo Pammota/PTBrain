a85b3258fe0fa46aac1d2499beeec06141cd303f
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-12 03:01:11 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -534,7 +534,7 @@ class BrainThread(Thread):
             # info for roundabout
             x0, y0 = 117, 10
             ref_thresh = 1
-            final_thresh = 5
+            final_thresh = 20
             isForward = True
             size_pixel = 500
             size_cm = 234
