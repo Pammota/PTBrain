8d5de0172efdc6b522aea64fa68e19a6f89419a8
flaviusfetean <flavius_fetean@yahoo.com>
2022-05-13 01:17:48 +0300

    vehicle validation conflict

--- Brain_Skeleton/data/localisationssystem/locsys.py
+++ Brain_Skeleton/data/localisationssystem/locsys.py
@@ -90,7 +90,7 @@ class LocalisationSystem(Thread):
 
         coords = self.__position_listener.coor
 
-        #print("Coordinates are: {}, {}".format(coords['coor'][0], coords['coor'][1]))
+        print("Coordinates are: {}, {}".format(coords['coor'][0], coords['coor'][1]))
         return coords
 
     def ID(self):
