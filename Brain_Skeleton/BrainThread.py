55b8fcedc7c734ace37b5276a231792c728728da
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-13 04:27:00 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -538,7 +538,7 @@ class BrainThread(Thread):
         if case == "roundabout_forward":
             # info for roundabout
             x0, y0 = 117, 10
-            v = 13
+            v = 10
             ref_thresh = 10
             final_thresh = 10
             isForward = False
