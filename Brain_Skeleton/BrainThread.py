436d906660385f5469fce92d7cbe05c589e73032
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-13 04:17:39 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -544,11 +544,11 @@ class BrainThread(Thread):
             isForward = False
             size_pixel = 500
             size_cm = 234
-            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=10, x_c=99, y_c=117, alpha_min=0,
+            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=6, x_c=99, y_c=117, alpha_min=0,
                                                                   alpha_max=1.2)
             for point in ref_points_aux:
                 ref_points.append(point)
-            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=10, x_c=99, y_c=117, alpha_min=5.05,
+            ref_points_aux = pathGenerator.generate_circle_points(r=67, d=6, x_c=99, y_c=117, alpha_min=5.05,
                                                                   alpha_max=6.28)
             for point in ref_points_aux:
                 ref_points.append(point)
