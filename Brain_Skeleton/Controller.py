9ff14c77cc3f01e547f19f4c8ee2dc9dace1e6fc
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-13 23:33:22 +0300

    Update Controller.py

--- Brain_Skeleton/Controller.py
+++ Brain_Skeleton/Controller.py
@@ -241,7 +241,7 @@ class Controller():
 
     def __localize(self, fulfilled=None):
         if not RANDOM_POSITION:
-            return PathPlanner(["A", "B", "C", "D"])
+            return PathPlanner(["I", "J", "G", "H"])
 
         start_con_time = time.time()
 
