45e429235656715cdce655a1945f4db84bf9b37d
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-14 06:02:00 +0300

    Update BrainThread.py

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -656,9 +656,9 @@ class BrainThread(Thread):
         lane_x = 30
         lane_y = 25
         # ref_points.append((x_obs -35, y_obs + lane_y))
-        ref_points.append((x - 35, y + 50))
-        ref_points.append((x_front - 30, y_front + 80))
-        end_point = (x_front + 15, y_front + 155)
+        ref_points.append((x - 40, y + 50))
+        ref_points.append((x_front - 40, y_front + 70))
+        end_point = (x_front + 15, y_front + 90)
         # ref_points.append(end_point)
 
         size_pixel = 300
