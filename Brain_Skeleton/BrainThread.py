f398df83b346dbeb770a8a0788e3e81bd645feea
flaviusfetean <flavius_fetean@yahoo.com>
2022-05-14 07:44:44 +0300

    parking maneuver

--- Brain_Skeleton/BrainThread.py
+++ Brain_Skeleton/BrainThread.py
@@ -312,6 +312,7 @@ class BrainThread(Thread):
             s_leep -= 0.01
         for index in range(r_ange):
             time.sleep(s_leep)
+            self.outP_com.send((theta_command, speed_command))
 
 
     def plot_timeframes_graph(self, timeframes):
